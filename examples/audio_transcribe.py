478f45d392a96b3386e18eea583766538b78685f
Aishwarya <aishwarya.anilkumar@outlook.com>
2022-07-03 22:04:58 -0400

    added a print command

--- examples/audio_transcribe.py
+++ examples/audio_transcribe.py
@@ -20,6 +20,7 @@ except sr.UnknownValueError:
     print("Sphinx could not understand audio")
 except sr.RequestError as e:
     print("Sphinx error; {0}".format(e))
+    print(" ")
 
 # recognize speech using Google Speech Recognition
 try:
